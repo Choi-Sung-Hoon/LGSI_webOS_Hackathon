253685d2a6a213c15795fe28ec295463ff91b7b8
Choi-Sung-Hoon <tjdgns9246@naver.com>
2019-07-15 03:17:56 +0900

    Rename directory name
    Rename directory name

--- LG/smartCarApp/googleMapService.js
+++ LG/smartCarApp/googleMapService.js
@@ -1,62 +1,40 @@
-
-window.onload = function() {
-    initialize();
-};
-
-//var smartCar;
-//var ambulance;
-var marker;
 var map;
 var geocoder;
-var infowindow = null;
 
-//form mall 위치 (임의의 초기 값)
-var latitude = 12.934674;
-var longitude = 77.611170;
+// create marker
+var myMarker;
+var ambulanceMarkers = new Array();
 
-function initialize() {
+function initialize()
+{
     geocoder = new google.maps.Geocoder();
-    var lat = latitude;
-    var lng = longitude;
-    setInitPosition(lat, lng);
-    setMarker(lat, lng);
 }
 
-function moveMarker(lat, lng) { 
-    var latv = parseFloat(lat);
-    var lonv = parseFloat(lng);
-    marker.setPosition(new google.maps.LatLng(latv, lonv));
-}
-
-function addMarker(lat, lng) { 
-    var latv = parseFloat(lat);
-    var lonv = parseFloat(lng);
-    //setCurrentPosition(latv, lonv);
-    setMarker(latv, lonv);
-}
-
-function setInitPosition(lat, lng) {
-    var latitude = lat;
-    var longitude = lng;
-
+function setMapPosition(lat, lng)
+{
     var mapOptions = {
-        zoom: 12,
+        center: new google.maps.LatLng(lat, lng),
+        zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        center: new google.maps.LatLng(latitude, longitude)
     };
-
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
 
-function setMarker(lat, lng) {
-    // 현재 위치 마커 생성
-    marker = new google.maps.Marker({
+function createMarker(lat, lng)
+{
+    var marker = new google.maps.Marker({
         position: new google.maps.LatLng(lat, lng),
         map: map,
         //draggable: false,
         //icon: "http://maps.google.com/mapfiles/ms/micons/man.png";
     });
-    marker.addListener('click', toggleBounce); //click evnet가 아니라 웹소켓에서 뭐가 날라오는 이벤트가 발생하면 저렇게 깜박이는 게 낫겠다.
+    //marker.addListener('click', toggleBounce); //click evnet가 아니라 웹소켓에서 뭐가 날라오는 이벤트가 발생하면 저렇게 깜박이는 게 낫겠다.
+    return marker;
+}
+
+function moveMarker(marker, lat, lng)
+{
+    marker.setPosition(new google.maps.LatLng(lat, lng));
 }
 
 /*
@@ -76,10 +54,10 @@ function drawCircle(lat, lng) {
 }
 */
 
-function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
+function toggleBounce()
+{
+    if (marker.getAnimation() !== null)
+		marker.setAnimation(null);
+	else
+		marker.setAnimation(google.maps.Animation.BOUNCE);
 }
---
+++ LG/smartCarApp/websocket.js
@@ -0,0 +1,146 @@
+var serverIp = "192.168.43.138";
+var serverPort = "9999";
+var wsUri = "ws://" + serverIp + ":" + serverPort;
+
+var deviceId = 1;			// this should be same with raspbian device,
+var deviceType = "webos"	// but different from webos device
+var deviceIp, deviceType, latitude, longitude;
+
+var ambulanceList;
+
+function createWebSocket()
+{
+	webSocket = new WebSocket(wsUri);
+	if(webSocket)
+	{
+		// add event functions
+		webSocket.onopen = function(event) { onOpen(event) };
+		webSocket.onclose = function(event) { onClose(event) };
+		webSocket.onmessage = function(event) { onMessage(event) };
+		webSocket.onerror = function(event) { onError(event) };
+	}
+    initialize();
+}
+
+// when websocket opens
+function onOpen(event)
+{
+	// var output = document.getElementById("output");
+	// output.innerHTML += "<br>Successfully connected to " + wsUri + "<br>";
+}
+
+// when websocket closes
+function onClose(event)
+{
+	// var output = document.getElementById("output");
+	// output.innerHTML += "<br>Socket is successfully closed<br>";
+}
+
+function onError(event)
+{
+	// var output = document.getElementById("output");
+	// output.innerHTML += "<br>Error ocurred!<br>";
+}
+
+// when a message is received from the server, parse it into JSON format output device_id and result on the screen
+function onMessage(event)
+{
+	var message = JSON.parse(event.data);
+	var output = document.getElementById("output");
+	output.innerHTML = "Message from server: <br>" + JSON.stringify(message, null, '\t').replace(/\n/g, '<br>') + "<br>";
+
+	switch(message.method)
+	{
+		case "requestInformation":
+		{
+			if(serverIp == message.payload.serverIp)
+			{
+				deviceIp = message.payload.deviceIp;
+				// send device information back to server
+				var message =
+				{
+					"deviceIp": deviceIp,
+					"method": "updateInformation",
+					"payload":
+					{
+						"deviceId": deviceId,
+						"deviceType": deviceType
+					}
+				}
+				webSocket.send(JSON.stringify(message));
+				// output.innerHTML = "Sent message to server: <br>" + JSON.stringify(message, null, '\t').replace(/\n/g, '<br>') + "<br>";
+			}
+			break;
+		}
+		case "updateLocation":
+		{
+			latitude = parseFloat(message.payload.latitude);
+			longitude = parseFloat(message.payload.longitude);
+			
+			if(myMarker == null)
+			{
+				setMapPosition(latitude, longitude);
+				myMarker = createMarker(latitude, longitude);
+			}
+			else
+				moveMarker(myMarker, latitude, longitude);
+
+			ambulanceList = message.payload.ambulanceList;
+			if (ambulanceList.length != 0) //엠뷸런스 on 상태
+			{
+				if (ambulanceMarkers.length == 0) //만들어진 마커가 없다는 거니까
+				{
+					for (var i = 0; i < ambulanceList.length; i++)
+						ambulanceMarkers[i] = createMarker(parseFloat(ambulanceList[i].latitude), parseFloat(ambulanceList[i].longitude));
+				}
+				else if(ambulanceMarkers.length < ambulanceList.length)
+				{
+					for (var i = 0; i < ambulanceList.length; i++)
+					{
+						if(i < ambulanceMarkers.length)
+							moveMarker(ambulanceMarkers[i], parseFloat(ambulanceList[i].latitude), parseFloat(ambulanceList[i].longitude));
+						else
+							ambulanceMarkers[i] = createMarker(parseFloat(ambulanceList[i].latitude), parseFloat(ambulanceList[i].longitude));
+					}
+				}
+				else
+				{
+					for (var i = 0; i < ambulanceList.length; i++)
+						moveMarker(ambulanceMarkers[i], parseFloat(ambulanceList[i].latitude), parseFloat(ambulanceList[i].longitude));
+				}
+			}
+			break;
+		}
+		case "sendNotification":
+		{
+			distance = message.payload.distance;
+			createToast("An ambulance is " + distance + "m away! Please make a way!");
+		}
+	}
+}
+
+function ambulanceOn()
+{
+	var message = 
+	{
+		"deviceIp": deviceIp,
+		"method": "ambulanceOn",
+		"payload": { "deviceId": deviceId }
+	}
+	webSocket.send(JSON.stringify(message));
+}
+
+function ambulanceOff()
+{
+	var message = 
+	{
+		"deviceIp": deviceIp,
+		"method": "ambulanceOff",
+		"payload": { "deviceId": deviceId }
+	}
+	webSocket.send(JSON.stringify(message));
+}
+
+// attach callback function to the event.
+// This should be done before load
+window.addEventListener("load", createWebSocket, false);
